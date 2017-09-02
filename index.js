96a5a7478f91f2acdf9dfba8d09cffa6fc42a1b1
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-02 04:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTO("thanksgiving", "Robert") {
+function happyHolidaysTO(thanksgiving, Robert) {
   return `Happy ${holiday}, ${name}!`
 }
